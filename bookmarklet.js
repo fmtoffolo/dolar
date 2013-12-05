c8599bc9d7e09ed14cecaf099d2ccbf0e7ff76a0
fmtoffolo <fmtoffolo+git@gmail.com>
2013-12-04 23:08:05 -0300

    First commit
    Just the bookmarklet and script

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {   
+    var jsCode = document.createElement('script');   
+    jsCode.setAttribute('src', 'http://localhost:8000/main.js');                    
+  document.body.appendChild(jsCode);   
+ }());  
